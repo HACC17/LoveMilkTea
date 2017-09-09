136562b59b09e9e61213b55142c29d7ad8df9867
Danny <dtan808@hawaii.edu>
2017-09-09 13:40:54 -1000

    Http call to google distance matrix successful

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, Injectable} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {MapPage} from "../map/map";
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -8,34 +10,43 @@ import {MapPage} from "../map/map";
     templateUrl: 'explore.html',
 })
 
+@Injectable()
 export class ExplorePage {
 
-    currentLocation: any;
+    currentLocation: string;
     currentLat: any;
     currentLng: any;
+    url: string;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-        if(navigator.geolocation) {
+    public expLocations: string =
+        "21.2989380,-157.8185730" + // Warrior Rec Center
+        "|21.2984350,-157.8188780" + // Campus Center
+        "|21.3004500,-157.8161520" + // Hamilton Library
+        "|21.2943550,-157.8186020" + // Stan Sheriff Center
+        "|21.2993160,-157.8150410" + // Kennedy Theatre
+        "|21.3008300,-157.8156720"   // Paradise Palms Cafe
+    ;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 this.currentLat = position.coords.latitude;
                 this.currentLng = position.coords.longitude;
-                this.currentLocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
+                this.currentLocation = this.currentLat + "," + this.currentLng;
+                console.log(this.currentLocation);
+                this.findDistanceAndDuration();
             })
         }
         else {
             console.log("Location blocked");
         }
-        console.log(this.currentLocation);
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad ExplorePage');
     }
 
-    mapTo(value){
+    mapTo(value) {
         this.navCtrl.push(MapPage, {
             locationIndex: value.toString()
         });
@@ -43,16 +54,28 @@ export class ExplorePage {
 
     // will check if app has access to user current location to calculate distance from point of interest
     hasCurrLocation() {
-        if(this.currentLocation){
-            console.log(this.currentLocation);
+        if (this.currentLocation) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
 
-    findDistance(){
+    //must add 'Allow-Control-Allow-Origin: *' Chrome plugin to local browser
+    findDistanceAndDuration() {
+        this.url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + this.currentLocation +
+                "&destinations=" + this.expLocations + "&mode=walking&units=imperial&key=" +
+                "AIzaSyCeP_xxvneWjyU_0EIg5slVUl3I6TtH4oA";
+        return new Promise(resolve => {
+            this.http.request(this.url)
+                .map(res => res.json()).subscribe(data => {
+                console.log(data);
 
+                
+
+                resolve(this.data);
+            });
+        });
     }
 }
