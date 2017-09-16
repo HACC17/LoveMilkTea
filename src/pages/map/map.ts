b4e256bd2facbc32a1a6c353d4f820c6bd0ddd3b
Danny <dtan808@hawaii.edu>
2017-09-15 17:05:13 -1000

    get current location upon map load

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -8,7 +8,6 @@ import {isNullOrUndefined} from "util";
 import * as Fuse from 'fuse.js';
 
 
-
 declare var google;
 // Array to contain Markers on the map
 let stash = [];
@@ -37,6 +36,7 @@ export class MapPage {
     exploreIndex2: any;
     currentLat: any;
     currentLng: any;
+    latLng: any;
     jsonData: any;
     directionsService: any;
     directionsDisplay: any;
@@ -48,6 +48,7 @@ export class MapPage {
     // Should we load location types from a config file?
     changeIcon: boolean = false;
     isSearching: boolean = false;
+    isInfoWindowOpen: boolean = false;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public loading: LoadingController, public http: Http) {
         this.exploreIndex = navParams.get('locationIndex');
@@ -68,15 +69,16 @@ export class MapPage {
     ionViewDidLoad() {
         this.loadTagData(); //we'll just load all data from firebase once
         this.loadMap();
+        this.getLatLng();
     }
 
-    searchPoints(input){
+    searchPoints(input) {
         this.isSearching = true;
         let fuse = new Fuse(this.searchList, this.fuseOptions)
 
         console.log(input);
         if (input === '') {
-           this.searchList = this.geoMarkers;
+            this.searchList = this.geoMarkers;
         } else {
             //console.log(fuse.search(input));
             this.searchList = fuse.search(input);
@@ -101,7 +103,7 @@ export class MapPage {
 
     //retrieves the tags from our firebase, populates them on map.
     loadTagData() {
-       // this.clearAllMarkers();
+        // this.clearAllMarkers();
         //load the tag data into the geoMarkers variable
         this.geoMarkers = [];
         this.ref.once("value")
@@ -127,8 +129,8 @@ export class MapPage {
                     this.createExpRoute();
                 }
                 else if (!this.exploreIndex && this.exploreIndex2) {
-                 this.addMarker(this.exploreIndex2);
-                 }
+                    this.addMarker(this.exploreIndex2);
+                }
 
                 this.searchList = this.geoMarkers.slice();
 
@@ -168,6 +170,12 @@ export class MapPage {
         });
 
         this.infoWindow.open(this.map, this.marker);
+        this.isInfoWindowOpen = true;
+
+        google.maps.event.addListener(this.infoWindow, 'closeclick', (() => {
+            this.isInfoWindowOpen = false;
+            this.clearStarterMarker();
+        }));
     }
 
     clearStarterMarker() {
@@ -266,7 +274,7 @@ export class MapPage {
 
     inStreetView() {
         if (this.panorama) {
-            if (this.panorama.getVisible()){
+            if (this.panorama.getVisible()) {
                 return true;
             }
             else {
@@ -318,7 +326,7 @@ export class MapPage {
                 google.maps.event.addListener(marker, 'click', (() => {
                     this.infoWindow.setContent(info);
                     this.infoWindow.open(this.map, marker);
-                }))
+                }));
             } else {
                 console.log("Category: " + criteria + " does not exist!");
             }
@@ -356,32 +364,32 @@ export class MapPage {
             this.changeIcon = false;
             console.log('this is clear all markers');
             console.log(stash);
-        }  else {
+        } else {
             console.log('Stash array does not exist!');
         }
     }
 
-    getInfoWindowData(location){
+    getInfoWindowData(location) {
 
         let infoContent = '<div class="ui grid">';
-        if(location.key){
+        if (location.key) {
             let imgSrc = "http://manoanow.org/app/map/images/" + location.key + ".png";
-            infoContent +='<img class="ui fluid image info" src="' + imgSrc + '">'
+            infoContent += '<img class="ui fluid image info" src="' + imgSrc + '">'
         }
-        if(location.name) {
-            infoContent +=  '<div id="windowHead">' + location.name + '</div>'
+        if (location.name) {
+            infoContent += '<div id="windowHead">' + location.name + '</div>'
         }
-        if(location.description) {
+        if (location.description) {
             infoContent += '<div id="description">' + location.description + '</div>'
         }
-        if(location.address) {
-            infoContent +=  '<div id="addressTitle">Address: ' + location.address + '</div>'
+        if (location.address) {
+            infoContent += '<div id="addressTitle">Address: ' + location.address + '</div>'
         }
-        if(location.number) {
-            infoContent +=   '<div id="phoneTitle">Phone: ' + location.number + '</div>' ;
+        if (location.number) {
+            infoContent += '<div id="phoneTitle">Phone: ' + location.number + '</div>';
         }
 
-        infoContent+=  '</div>';
+        infoContent += '</div>';
 
         return infoContent;
     }
@@ -416,26 +424,25 @@ export class MapPage {
         }
     }
 
+    getLatLng() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.currentLat = position.coords.latitude;
+                this.currentLng = position.coords.longitude;
+                this.latLng = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+            });
+        }
+    }
+
     //Use HTML5 geolocation to get current lat/lng and place marker there
     showCurrLocation() {
-        this.loader = this.loading.create({
-            content: "Getting Coordinates..."
-        })
-        if (navigator.geolocation) {
-            this.loader.present().then(() => {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    this.currentLat = position.coords.latitude;
-                    this.currentLng = position.coords.longitude;
-                    var latLng = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                    this.userMarker.setPosition(latLng);
-                    this.userMarker.setMap(this.map);
-                    this.map.setCenter(latLng);
-                    this.loader.dismiss();
-                })
-            })
+        if (this.latLng) {
+            this.userMarker.setPosition(this.latLng);
+            this.userMarker.setMap(this.map);
+            this.map.setCenter(this.latLng);
         }
     }
 
@@ -718,8 +725,8 @@ export class MapPage {
             title: 'University of Hawaii at Manoa',
             icon: {
                 //directions walk
-                path:'M13.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM9.8 8.9L7 23h2.1l1.8-8 2.1 2v6h2v-7.5l-2.1-2 .6-3C14.8 12 16.8 13 19 13v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1-.3 0-.5.1-.8.1L6 8.3V13h2V9.6l1.8-.7',
-                fillColor: 'lightgreen',
+                path: 'M13.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM9.8 8.9L7 23h2.1l1.8-8 2.1 2v6h2v-7.5l-2.1-2 .6-3C14.8 12 16.8 13 19 13v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1-.3 0-.5.1-.8.1L6 8.3V13h2V9.6l1.8-.7',
+                fillColor: '#1B9A74',
                 strokeColor: 'darkgreen',
                 fillOpacity: 0.8, // you need this defined, there are no defaults.
                 scale: 1.75
@@ -739,63 +746,63 @@ export class MapPage {
 
     // holds icon SVG data and styling.
     icons = {
-        food :{
+        food: {
             //spoon and fork
-            path:'M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z',
+            path: 'M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z',
             fillColor: '#FF6699',
             strokeColor: '#CA3157',
             fillOpacity: 0.8, // you need this defined, there are no defaults.
         },
-        drink :{
+        drink: {
             //drink glass
-            path:'M6 4l4.03 36.47C10.26 42.46 11.95 44 14 44h20c2.05 0 3.74-1.54 3.97-3.53L42 4H6zm18 34c-3.31 0-6-2.69-6-6 0-4 6-10.8 6-10.8S30 28 30 32c0 3.31-2.69 6-6 6zm12.65-22h-25.3l-.88-8h27.07l-.89 8z',
+            path: 'M6 4l4.03 36.47C10.26 42.46 11.95 44 14 44h20c2.05 0 3.74-1.54 3.97-3.53L42 4H6zm18 34c-3.31 0-6-2.69-6-6 0-4 6-10.8 6-10.8S30 28 30 32c0 3.31-2.69 6-6 6zm12.65-22h-25.3l-.88-8h27.07l-.89 8z',
             fillColor: '#FF6699',
             strokeColor: '#CA3157',
             fillOpacity: 0.8,
         },
-        classroom : {
+        classroom: {
             //school
             path: 'M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z',
             fillColor: '#007c34',
             strokeColor: 'darkgreen',
             fillOpacity: 0.8,
         },
-        entertainment : {
+        entertainment: {
             //mood
             path: 'M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
             fillColor: '#4E2683',
             strokeColor: '#4E2683',
             fillOpacity: 0.8,
         },
-        housing : {
+        housing: {
             //home
             path: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z',
             fillColor: '#FFD403',
             strokeColor: '#FFB804',
             fillOpacity: 0.8,
         },
-        library : {
+        library: {
             //book
             path: 'M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 4h5v8l-2.5-1.5L6 12V4z',
             fillColor: '#D20104',
             strokeColor: '#8B0000',
             fillOpacity: 0.8,
         },
-        parking : {
+        parking: {
             //local parking
             path: 'M13 3H6v18h4v-6h3c3.31 0 6-2.69 6-6s-2.69-6-6-6zm.2 8H10V7h3.2c1.1 0 2 .9 2 2s-.9 2-2 2z',
             fillColor: '#D20104',
             strokeColor: '#8B0000',
             fillOpacity: 0.8,
         },
-        recreational : {
+        recreational: {
             //local event
             path: 'M20 12c0-1.1.9-2 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-1.99.9-1.99 2v4c1.1 0 1.99.9 1.99 2s-.89 2-2 2v4c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-4c-1.1 0-2-.9-2-2zm-4.42 4.8L12 14.5l-3.58 2.3 1.08-4.12-3.29-2.69 4.24-.25L12 5.8l1.54 3.95 4.24.25-3.29 2.69 1.09 4.11z',
             fillColor: '#006ECE',
             strokeColor: '#01008A',
             fillOpacity: 0.8,
         },
-        service : {
+        service: {
             //business center
             path: 'M10 16v-1H3.01L3 19c0 1.11.89 2 2 2h14c1.11 0 2-.89 2-2v-4h-7v1h-4zm10-9h-4.01V5l-2-2h-4l-2 2v2H4c-1.1 0-2 .9-2 2v3c0 1.11.89 2 2 2h6v-2h4v2h6c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2zm-6 0h-4V5h4v2z',
             fillColor: '#FF6600',
