1039facac24a4458cc7b74ff4fe4e09d3490fb3f
Tyler Chong <tylrchong@gmail.com>
2017-09-17 22:10:11 -1000

    enableProdMode()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import { MapPage }  from '../pages/map/map';
 import { LoginPage } from '../pages/login/login';
 import { ExplorePage } from "../pages/explore/explore";
 import { SubmitDataLandingPage } from '../pages/submit-data-landing/submit-data-landing';
+import { enableProdMode } from '@angular/core';
+
 declare function require(name:string);
 const ua = require('universal-analytics');
+
+enableProdMode();
 
 @Component({
     templateUrl: 'app.html'
