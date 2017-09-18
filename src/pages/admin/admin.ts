e44dd52e361f462432059aa777c1e767729486be
Danny <dtan808@hawaii.edu>
2017-09-17 21:26:42 -1000

    Merge branch 'master' into danny

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
-import {AngularFireAuth} from "angularfire2/auth";
-import {FIREBASE_CONFIG} from "./../../app.firebase.config";
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import { FIREBASE_CONFIG } from "./../../app.firebase.config";
 import * as firebase from 'firebase';
 
 @IonicPage()
@@ -28,20 +28,20 @@ export class AdminPage {
     }
 
     ionViewDidLoad() {
-        //added this here instead of constructor, better coding practice to put here?
-        var item = [];
+        const item = [];
         this.userInputRef.once('value').then(function (datakey) {
             datakey.forEach(function (data) {
-                var temp = data.val();
-                Object.assign(temp, {'key': data.key});
+                const temp = data.val();
+                Object.assign(temp, {
+                    'key': data.key
+                });
                 item.push(temp);
             });
         });
         this.items = item;
     }
 
-    //value is the key for the entry
-    approve(value) {
+    approve(value) { // 'value' is the key for the entry
         this.userInputRef.child(value.key).update({'status': 'approved'});
         this.masterDataRef = this.masterDataRef.push();
         this.masterDataRef.set({
@@ -63,22 +63,19 @@ export class AdminPage {
     }
 
     editData(value) {
-        console.log(value);
         this.navCtrl.push('EditSubmitDataPage', value);
     }
 
-    //value is the key for the entry
-    deleteItem(value) {
+    deleteItem(value) { // 'value' is the key for the entry
         this.userInputRef.child(value.key).remove();
-        //refresh page
-        this.navCtrl.setRoot(this.navCtrl.getActive().component);
+        this.navCtrl.setRoot(this.navCtrl.getActive().component); // refresh the page
     }
     filterItems(value){
-        var item = [];
+        const item = [];
         this.userInputRef.once('value').then(function (datakey) {
             datakey.forEach(function (data) {
-                var temp = data.val();
-                if (value === 'showAll'){
+                const temp = data.val();
+                if (value === 'showAll') {
                     item.push(temp);
                 } else if (temp.status === value){
                     item.push(temp);
