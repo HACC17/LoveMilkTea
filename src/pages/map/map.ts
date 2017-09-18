c9b63e3a4c9fe9191f14e61baa5b2857da743609
Danny <dtan808@hawaii.edu>
2017-09-17 15:46:21 -1000

    Bug fix

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -547,14 +547,14 @@ export class MapPage {
 
             google.maps.event.addListener(this.marker, 'click', (() => {
                 this.infoWindow.setContent(info);
+                this.marker.setPosition({lat: data.lat, lng: data.lng});
+                this.marker.setIcon(this.icons[data.type]);
                 this.infoWindow.open(this.map, this.marker);
                 document.getElementById("infoIcon").addEventListener("click", ()=>{
                     this.navCtrl.push("PointsPage", data);
                 });
             }))
                 this.endValue = latLng;
-                this.marker.setPosition({lat: data.lat, lng: data.lng});
-                this.infoWindow.open(this.map, this.marker);
                 this.isInfoWindowOpen = true;
 
             google.maps.event.addListener(this.infoWindow, 'closeclick', (() => {
